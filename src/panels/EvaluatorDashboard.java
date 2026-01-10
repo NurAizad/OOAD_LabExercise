79b94dd204211de9ea54cc00a5ba1320ead75085
NurAizad <nuraizadmurtadha@gmail.com>
2026-01-11 01:00:29 +0800

    update colours to evaluator dashboard

--- src/panels/EvaluatorDashboard.java
+++ src/panels/EvaluatorDashboard.java
@@ -2,7 +2,7 @@ package panels;
 
 import java.awt.*;
 import javax.swing.*;
-import javax.swing.border.Border;
+//import javax.swing.border.Border;
 
 import java.awt.event.*;
 
@@ -11,7 +11,7 @@ public class EvaluatorDashboard extends JPanel
     public EvaluatorDashboard(CardLayout cardLayout, JPanel cardManager)
     //public EvaluatorDashboard()
     {
-        //setLayout(new GridBagLayout());
+        setBackground(Color.LIGHT_GRAY);
         setLayout (new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.anchor = GridBagConstraints.CENTER;
@@ -20,31 +20,39 @@ public class EvaluatorDashboard extends JPanel
         centerContainer.setLayout(new BoxLayout(centerContainer, BoxLayout.Y_AXIS));
         add(centerContainer, gbc);
 
+
         //BUTTONS
         Dimension buttonSize = new Dimension(200, 30);
+        Color buttonColor = new Color(216, 223, 231);
 
         //REVIEW SUBMISSIONS BUTTON
         JPanel reviewButtonPanel = new JPanel();
         reviewButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        reviewButtonPanel.setBackground(Color.LIGHT_GRAY);
         centerContainer.add(reviewButtonPanel);
 
         JButton reviewButton = new JButton("View Presentations");
         reviewButton.setPreferredSize(buttonSize);
+        reviewButton.setBackground(buttonColor);
         reviewButtonPanel.add(reviewButton);
 
         //EVALUATE SUBMISSIONS BUTTON
         JPanel evaluateButtonPanel = new JPanel();
         evaluateButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        evaluateButtonPanel.setBackground(Color.LIGHT_GRAY);
         centerContainer.add(evaluateButtonPanel);
 
         JButton evaluateButton = new JButton("Evaluate Presentations");
         evaluateButton.setPreferredSize(buttonSize);
+        evaluateButton.setBackground(buttonColor);
         evaluateButtonPanel.add(evaluateButton);
 
         //LOGOUT BUTTON
         JButton logoutButton = new JButton("Logout");
         logoutButton.setPreferredSize(buttonSize);
+        logoutButton.setBackground(buttonColor);
         JPanel logoutButtonPanel = new JPanel();
+        logoutButtonPanel.setBackground(Color.LIGHT_GRAY);
 
         logoutButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         centerContainer.add(logoutButtonPanel);
