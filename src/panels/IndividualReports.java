db1128d48504dda9744f9b69d41481479f3b51ed
NurAizad <nuraizadmurtadha@gmail.com>
2026-02-07 13:13:29 +0800

    done decoraitng

--- src/panels/IndividualReports.java
+++ src/panels/IndividualReports.java
@@ -2,6 +2,8 @@ package panels;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.event.*;
 import java.io.File;
 import java.util.Scanner;
@@ -11,24 +13,52 @@ public class IndividualReports extends JPanel {
     private JComboBox<String> studentDropdown;
 
     public IndividualReports(CardLayout cardLayout, JPanel cardManager) {
+        
+        setBackground(new Color(245,245,245));
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.fill = GridBagConstraints.HORIZONTAL;
+        //gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(20, 20, 20, 20);
 
 
+        JPanel centerContainer = new JPanel();
+        centerContainer.setLayout(new BoxLayout(centerContainer, BoxLayout.Y_AXIS));
+        centerContainer.setBackground(Color.WHITE);
+        centerContainer.setPreferredSize(new Dimension (600, 500));
+        centerContainer.setBorder(BorderFactory.createCompoundBorder
+            (
+                new LineBorder(new Color(200, 200, 200), 1),
+                BorderFactory.createEmptyBorder(20, 30, 20, 30)
+            ));
+        add(centerContainer, gbc);
+
+        Color buttonColor = new Color(216, 223, 231);
         //TITLE -----------------------------------------------------------------------------------------------
 
         JPanel indiRepPanel = new JPanel();
-        indiRepPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        //indiRepPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        indiRepPanel.setLayout(new BoxLayout(indiRepPanel, BoxLayout.Y_AXIS));
+        indiRepPanel.setBackground(Color.WHITE);
         JLabel indiRepLabel = new JLabel("INDIVIDUAL REPORTS");
         indiRepLabel.setFont(new Font("Arial", Font.BOLD, 24));
-        indiRepPanel.add(indiRepLabel);
+        indiRepLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        centerContainer.add(indiRepLabel);
 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(20, 0, 30, 0);
-        add(indiRepPanel, gbc);
+        //indiRepPanel.add(indiRepLabel);
+
+        //gbc.gridx = 0;
+       // gbc.gridy = 0;
+       // gbc.gridwidth = 1;
+        //gbc.insets = new Insets(20, 0, 30, 0);
+        // indiRepPanel.setBorder(BorderFactory.createCompoundBorder
+        //     (
+        //         new LineBorder(new Color(200, 200, 200), 1),
+        //         BorderFactory.createEmptyBorder(20, 30, 20, 30)
+        //     ));
+        
+        // add(indiRepPanel, gbc);
 
 
         //STUDENT ----------------------------------------------------------------------------------------------
@@ -36,7 +66,7 @@ public class IndividualReports extends JPanel {
         JPanel studentPanel = new JPanel(new GridBagLayout());
         GridBagConstraints studentGbc = new GridBagConstraints();
         studentGbc.insets = new Insets(0, 0, 0, 10);
-        studentGbc.fill = GridBagConstraints.HORIZONTAL;
+        //studentGbc.fill = GridBagConstraints.HORIZONTAL;
 
         JLabel studentLabel = new JLabel("Student Name:");
         studentLabel.setFont(new Font("Arial", Font.PLAIN, 16));
@@ -46,44 +76,52 @@ public class IndividualReports extends JPanel {
 
         studentDropdown = new JComboBox<>();
         insertStudentNames();
-        studentDropdown.setPreferredSize(new Dimension(250, 25));
+        studentDropdown.setPreferredSize(new Dimension(200, 25));
+        studentDropdown.setBackground(buttonColor);
         studentGbc.gridx = 1;
         studentGbc.gridy = 0;
-        studentGbc.weightx = 1.0; // let it expand
+        //studentGbc.weightx = 1.0; // let it expand
         studentPanel.add(studentDropdown, studentGbc);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.insets = new Insets(0, 0, 10, 0);
-        add(studentPanel, gbc);
+        centerContainer.add(studentPanel);
+        //add(studentPanel, gbc);
 
 
         //VIEW REPORT ------------------------------------------------------------------------------------------
 
         JPanel reportButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JButton viewReportButton = new JButton("View Report");
+        viewReportButton.setBackground(buttonColor);
 
         viewReportButton.setPreferredSize(null);
         reportButtonPanel.add(viewReportButton);
+        centerContainer.add(reportButtonPanel);
 
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.insets = new Insets(20, 0, 20, 0);
-        add(reportButtonPanel, gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 2;
+        // gbc.insets = new Insets(20, 0, 20, 0);
+        //add(reportButtonPanel, gbc);
+        //centerContainer.add(reportButtonPanel, gbc);
 
 
         //BACK BUTTONS -----------------------------------------------------------------------------------------
 
         JPanel bottomButtonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
         JButton backToOverviewButton = new JButton("Back to Overview");
+        backToOverviewButton.setBackground(buttonColor);
         JButton backToDashboardButton = new JButton("Back to Dashboard");
+        backToDashboardButton.setBackground(buttonColor);
         bottomButtonPanel.add(backToOverviewButton);
         bottomButtonPanel.add(backToDashboardButton);
 
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.insets = new Insets(20, 0, 20, 0);
-        add(bottomButtonPanel, gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 3;
+        // gbc.insets = new Insets(20, 0, 20, 0);
+        centerContainer.add(bottomButtonPanel);
+        //add(bottomButtonPanel, gbc);
 
         
         //ACTION LISTENERS --------------------------------------------------------------------------------------
--- src/panels/Overview.java
+++ src/panels/Overview.java
@@ -2,20 +2,35 @@ package panels;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.event.*;
 
 public class Overview extends JPanel
 {
     public Overview(CardLayout cardLayout, JPanel cardManager)
     {
+        setBackground(new Color(245,245,245));
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.CENTER;
+        //gbc.anchor = GridBagConstraints.CENTER;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(20, 20, 20, 20);
 
         JPanel centerContainer = new JPanel();
         centerContainer.setLayout(new BoxLayout(centerContainer, BoxLayout.Y_AXIS));
-        gbc.insets = new Insets(100, 0, 0, 0);
+        centerContainer.setBackground(Color.WHITE);
+        //centerContainer.setPreferredSize(new Dimension (600, 500));
+        centerContainer.setBorder(BorderFactory.createCompoundBorder
+            (
+                new LineBorder(new Color(200, 200, 200), 1),
+                BorderFactory.createEmptyBorder(20, 30, 20, 30)
+            ));
         add(centerContainer, gbc);
+
+        Color buttonColor = new Color(216, 223, 231);
 
         //TITLE ----------------------------------------------------------------------------------------------
         JLabel overviewLabel = new JLabel("OVERVIEW", SwingConstants.CENTER);
@@ -23,7 +38,7 @@ public class Overview extends JPanel
         overviewLabel.setFont(new Font("Arial", Font.BOLD, 24));
         centerContainer.add(overviewLabel);
 
-        centerContainer.add(Box.createRigidArea(new Dimension(0, 20)));
+        //centerContainer.add(Box.createRigidArea(new Dimension(0, 20)));
 
 
         //BUTTONS ---------------------------------------------------------------------------------------------
@@ -33,36 +48,42 @@ public class Overview extends JPanel
         //INDIVIDUAL REPORTS BUTTON
         JPanel individualButtonPanel = new JPanel();
         individualButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        centerContainer.add(individualButtonPanel);
+        //centerContainer.add(individualButtonPanel);
 
         JButton individualReportsButton = new JButton("Individual Reports");
+        individualReportsButton.setBackground(buttonColor);
         individualReportsButton.setPreferredSize(buttonSize);
         individualButtonPanel.add(individualReportsButton);
+        centerContainer.add(individualButtonPanel);
 
         //OVERALL SUMMARY BUTTON
         JPanel summaryButtonPanel = new JPanel();
         summaryButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        centerContainer.add(summaryButtonPanel);
+        //centerContainer.add(summaryButtonPanel);
 
         JButton overallSummaryButton = new JButton("Overall Summary");
+        overallSummaryButton.setBackground(buttonColor);
         overallSummaryButton.setPreferredSize(buttonSize);
         summaryButtonPanel.add(overallSummaryButton);
+        centerContainer.add(summaryButtonPanel);
 
         //BACK TO DASHBOARD 
         JPanel bottomButtonPanel = new JPanel();
         bottomButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         
         JButton backToDashboardButton = new JButton("Back to Dashboard");
+        backToDashboardButton.setBackground(buttonColor);
         backToDashboardButton.setPreferredSize(new Dimension(200, 30));
         bottomButtonPanel.add(backToDashboardButton);
+        centerContainer.add(bottomButtonPanel);
 
-        GridBagConstraints gbcBottom = new GridBagConstraints();
-        gbcBottom.gridx = 0;
-        gbcBottom.gridy = 1; 
-        gbcBottom.weightx = 1.0;
-        gbcBottom.weighty = 1.0; 
-        gbcBottom.anchor = GridBagConstraints.PAGE_END;
-        add(bottomButtonPanel, gbcBottom);
+        // GridBagConstraints gbcBottom = new GridBagConstraints();
+        // gbcBottom.gridx = 0;
+        // gbcBottom.gridy = 1; 
+        // gbcBottom.weightx = 1.0;
+        // gbcBottom.weighty = 1.0; 
+        // gbcBottom.anchor = GridBagConstraints.PAGE_END;
+        // add(bottomButtonPanel, gbcBottom);
 
         //ACTION LISTENERS (to add: destinationss)
         individualReportsButton.addActionListener(new ActionListener() 
