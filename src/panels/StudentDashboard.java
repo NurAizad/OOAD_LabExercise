e039813f080b0cea5c8274360cc054184c9e4afd
NurAizad <nuraizadmurtadha@gmail.com>
2026-02-09 01:19:59 +0800

    removed vote best rpesetner in stud das oard

--- src/panels/StudentDashboard.java
+++ src/panels/StudentDashboard.java
@@ -59,14 +59,14 @@ public class StudentDashboard extends JPanel{
 
         //VOTING
 
-        JPanel votePanel = new JPanel();
-        votePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        centerContainer.add(votePanel);
+        // JPanel votePanel = new JPanel();
+        // votePanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        // centerContainer.add(votePanel);
 
-        JButton voteButton = new JButton("Vote Best Presenter");
-        voteButton.setPreferredSize(buttonSize);
-        voteButton.setBackground(buttonColor);
-        votePanel.add(voteButton);
+        // JButton voteButton = new JButton("Vote Best Presenter");
+        // voteButton.setPreferredSize(buttonSize);
+        // voteButton.setBackground(buttonColor);
+        // votePanel.add(voteButton);
 
         // Research title
 
